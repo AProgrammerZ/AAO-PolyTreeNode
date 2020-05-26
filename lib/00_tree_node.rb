483519b35e979943e20fb4ea0821f0307c6315e2
Aharon <aharonzarnighian@gmail.com>
2020-05-26 13:07:55 -0400

    Finished Phase 4

--- lib/00_tree_node.rb
+++ lib/00_tree_node.rb
@@ -22,4 +22,24 @@ class PolyTreeNode
         child_node.parent=(nil)
         @children.delete(child_node)
     end
+
+    def dfs(target_value)
+        return self if self.value == target_value
+        @children.each do |child|
+            search_result = child.dfs(target_value)
+            return search_result unless search_result.nil?
+        end
+        nil
+    end
+
+    def bfs(target_value)
+        queue = [self]
+
+        until queue.empty?
+            first_node = queue.shift
+            return first_node if first_node.value == target_value
+            first_node.children.each { |child| queue << child }
+        end
+        nil
+    end
 end
