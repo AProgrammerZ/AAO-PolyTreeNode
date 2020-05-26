2bc7706a8605cf2ab70c62362048e17e5d3aaf3e
Aharon <aharonzarnighian@gmail.com>
2020-05-26 11:19:55 -0400

    Finished Phase 1

--- lib/00_tree_node.rb
+++ lib/00_tree_node.rb
@@ -1,5 +1,15 @@
 class PolyTreeNode
+    attr_reader :parent, :children, :value
+    
     def initialize(value)
-        
+        @value = value
+        @parent = nil
+        @children = []
+    end
+
+    def parent=(value)
+        # @parent.children.delete(self)
+        @parent = value
+        @parent.children << self unless value == nil
     end
 end
